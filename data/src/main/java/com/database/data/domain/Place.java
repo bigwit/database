97da623d036f175002f9bc09c1863e8286740ce4
nikit <nikitakur1993@gmail.com>
2014-05-06 01:24:47 +0400

    Добавлены конфиги, поправлены косяки в data layer

--- data/src/main/java/com/database/data/domain/Place.java
+++ data/src/main/java/com/database/data/domain/Place.java
@@ -1,76 +1,76 @@
-package com.database.data.domain;
-
-import javax.persistence.*;
-
-import org.hibernate.mapping.Set;
-
-@Entity
-@Table(name = "places")
-@NamedQueries({
-	@NamedQuery(name = "Place.findAll", query = "select p from Place p"),
-	@NamedQuery(name = "Place.findById", query = "select p from Place p right join fetch p.places where p.id = :id")
-})
-public class Place {
-
-	@Id
-	@Column(name = "id")
-	private Long id;
-	
-	@Column(name = "name")
-	private String name;
-	
-	@Column(name = "description")
-	private String description;
-	
-	@OneToMany
-	@JoinColumn(name = "id_location")
-	private Location location;
-	
-	@ManyToOne
-	@JoinColumn(name = "id_place")
-	private Set places;
-	
-	public Place() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Location getLocation() {
-		return location;
-	}
-
-	public void setLocation(Location location) {
-		this.location = location;
-	}
-
-	public Set getPlaces() {
-		return places;
-	}
-
-	public void setPlaces(Set places) {
-		this.places = places;
-	}
-	
-}
+//package com.database.data.domain;
+//
+//import javax.persistence.*;
+//
+//import org.hibernate.mapping.Set;
+//
+//@Entity
+//@Table(name = "places")
+//@NamedQueries({
+//	@NamedQuery(name = "Place.findAll", query = "select p from Place p"),
+//	@NamedQuery(name = "Place.findById", query = "select p from Place p right join fetch p.places where p.id = :id")
+//})
+//public class Place {
+//
+//	@Id
+//	@Column(name = "id")
+//	private Long id;
+//	
+//	@Column(name = "name")
+//	private String name;
+//	
+//	@Column(name = "description")
+//	private String description;
+//	
+////	@ManyToOne
+//	@JoinColumn(name = "id_location")
+//	private Location location;
+//	
+////	@OneToMany
+//	@JoinColumn(name = "id_place")
+//	private Set places;
+//	
+//	public Place() {
+//	}
+//
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public Location getLocation() {
+//		return location;
+//	}
+//
+//	public void setLocation(Location location) {
+//		this.location = location;
+//	}
+//
+//	public Set getPlaces() {
+//		return places;
+//	}
+//
+//	public void setPlaces(Set places) {
+//		this.places = places;
+//	}
+//	
+//}
