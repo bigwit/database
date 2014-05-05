94f240a8e4bea4e4f19634a4bf2586021c1d6eac
apismak <apismak@marathonbet.ru>
2014-05-06 01:41:45 +0400

    fix bad initialization

--- web/src/com/database/web/services/OfficesService.java
+++ web/src/com/database/web/services/OfficesService.java
@@ -2,25 +2,20 @@ package com.database.web.services;
 
 import java.util.List;
 
-import com.database.data.jpa.OfficeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.database.data.jpa.impl.OfficeServiceImpl;
 import com.database.data.domain.Office;
 import com.database.web.services.stereotypes.OfficesProvider;
 
 public class OfficesService implements OfficesProvider {
 
-	private OfficeService os;
+	@Autowired
+	private OfficeServiceImpl os;
 	
 	@Override
 	public List<Office> getAllOffices() {
-		return getOfficeService().findAll();
-	}
-	
-	private OfficeService getOfficeService() {
-		if(os == null) {
-			os = new OfficeServiceImpl();
-		}
-		return os;
+		return os.findAll();
 	}
 	
 }
