a67342126933baf57e2d8ef8a8ce76ad89b36ecf
nikit <nikitakur1993@gmail.com>
2014-05-14 01:50:38 +0400

    Добавлено нечто

--- web/target/database-0.0.1-SNAPSHOT/resources/js/main.js
+++ web/target/database-0.0.1-SNAPSHOT/resources/js/main.js
@@ -2,7 +2,9 @@
 	
 	$(document).ready(function() {
 		
+		window.initWindowEngine();
 		var message = document.getElementById("message").innerHTML.trim();
+		
 		if(message != null && message != "") {
 			window.setTimeout(function() {
 				$("#message").show('slow');
@@ -17,6 +19,21 @@
 				var uri = $(elem).attr('id');
 				window.location.href = uri;
 			});
+		});
+		
+		$("#_devs").click(function(e) {
+			e.event.preventDefault();
+			window.GrEngine.show("__stud_devs_", "Разработчики приложения");
+		});
+
+		$("#_lic").click(function(e) {
+			e.preventDefault();
+			window.GrEngine.show("__stud_lic_", "Лицензия настоящего ПО");
+		});
+
+		$("#_clb").click(function(e) {
+			e.preventDefault();
+			window.GrEngine.show("__stud_clb_", "Скромная служба поддержки");
 		});
 		
 	});
