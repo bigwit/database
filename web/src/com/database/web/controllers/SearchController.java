6304728d39d2bd1988856a61018f8a43ddd1272d
apismak <apismak@marathonbet.ru>
2014-05-21 10:51:01 +0400

    добавлен try-catch на ошибки при поиске

--- web/src/com/database/web/controllers/SearchController.java
+++ web/src/com/database/web/controllers/SearchController.java
@@ -22,46 +22,58 @@ import com.database.web.forms.ViewSearchForm;
 
 @Controller
 public class SearchController {
-	
+
 	private static final String NOT_FOUND_MESSAGE = "По вашему запросу '%s' ничего не найдено";
-	
+
 	@Autowired
 	private PageContextBean pageContextBean;
-	
+
 	@Autowired
 	private SiteContent siteContent;
-	
+
 	@Autowired
 	private SearchService searchService;
-	
+
 	@RequestMapping(value = "/sch", method = RequestMethod.POST)
-	public ModelAndView search(@ModelAttribute("query") ViewSearchForm query, 
-			ModelMap model, HttpServletRequest request) {		
+	public ModelAndView search(@ModelAttribute("query") ViewSearchForm query,
+			ModelMap model, HttpServletRequest request) {
 		ModelAndView view = new ModelAndView(Modeller.ROOT_VIEW);
 		pageContextBean.setContent(siteContent.getSearchPage());
 		Modeller.addDefaultModels(view, pageContextBean, request);
 		model.addAllAttributes(view.getModelMap());
-		
-		if(query != null && query.getQuery() != null && !query.getQuery().isEmpty()) {
-			List<TourInfo> results = searchService.search(query.getQuery(), getSearchTypeByQuery(query));
-			if(results == null || results.size() == 0) {
-				Modeller.addMessage(view, String.format(NOT_FOUND_MESSAGE, query.getQuery()));
-			} else {
-				view.addObject("valueQuery", query.getQuery());
-				view.addObject("resultset", results);
+		try {
+			if (query != null && query.getQuery() != null
+					&& !query.getQuery().isEmpty()) {
+				List<TourInfo> results = searchService.search(query.getQuery(),
+						getSearchTypeByQuery(query));
+				if (results == null || results.size() == 0) {
+					Modeller.addMessage(view,
+							String.format(NOT_FOUND_MESSAGE, query.getQuery()));
+				} else {
+					view.addObject("valueQuery", query.getQuery());
+					view.addObject("resultset", results);
+				}
 			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			Modeller.addMessage(view, "Не удалось связаться с базой данных");
 		}
 		return view;
 	}
-	
+
 	private SearchType getSearchTypeByQuery(ViewSearchForm form) {
-		switch(form.getType()) {
-			case "simple" : return SearchType.SIMPLE;
-			case "algo1" : return SearchType.LEVENSTEIN;
-			case "algo2" : return SearchType.VINKLER;
-			case "algo3" : return SearchType.SIMPLE;
-				default: return SearchType.SIMPLE;
+		switch (form.getType()) {
+		case "simple":
+			return SearchType.SIMPLE;
+		case "algo1":
+			return SearchType.LEVENSTEIN;
+		case "algo2":
+			return SearchType.VINKLER;
+		case "algo3":
+			return SearchType.SIMPLE;
+		default:
+			return SearchType.SIMPLE;
 		}
 	}
-	
+
 }
